4b9c7cd811d0b20cb6f6c71acb6c6ed96fb0c3e2
Ryan Eikanger <reikanger@gmail.com>
2024-06-03 17:25:44 -0500

    finished data modeling

--- data_modeling.sql
+++ data_modeling.sql
@@ -1,9 +1,27 @@
 drop table if exists departments, dept_emp, dept_manager, employees, salaries, titles cascade;
 
+create table titles (
+	title_id varchar(5) not null,
+	title varchar(50),
+	primary key (title_id)
+);
+
 create table departments (
-	dept_no varchar(5) not null primary key,
+	dept_no varchar(5) not null,
 	dept_name varchar(50),
 	primary key (dept_no)
+);
+
+create table employees (
+	emp_no int not null,
+	emp_title_id varchar(5) not null,
+	birth_date varchar(20),
+	first_name varchar(20),
+	last_name varchar(20),
+	sex varchar(1),
+	hire_date varchar(20),
+	primary key (emp_no),
+	foreign key (emp_title_id) references titles(title_id)
 );
 
 create table dept_emp (
@@ -15,25 +33,13 @@ create table dept_emp (
 
 create table dept_manager (
 	dept_no varchar(5) not null,
-	emp_no int not null
-);
-
-create table employees (
 	emp_no int not null,
-	emp_title_id varchar(5) not null,
-	birth_date varchar(20),
-	first_name varchar(20),
-	last_name varchar(20),
-	sex varchar(1),
-	hire_date varchar(20)
+	foreign key (dept_no) references departments(dept_no),
+	foreign key (emp_no) references employees(emp_no)
 );
 
 create table salaries (
 	emp_no int not null,
-	salary int
-);
-
-create table titles (
-	title_id varchar(5) not null,
-	title varchar(50)
+	salary int,
+	foreign key (emp_no) references employees(emp_no)
 );
